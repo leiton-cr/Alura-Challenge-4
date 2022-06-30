ca1dff04c402165d62a4ec6b2386fe6c380b0090
leiton-cr <80017848+leiton-cr@users.noreply.github.com>
2022-06-29 19:49:23 -0600

    Update templating.js

--- templating.js
+++ templating.js
@@ -4,7 +4,7 @@ fragments.forEach(fragment => {
     const htmlElement = document.getElementById(fragment);
     if (!htmlElement) return;
   
-    fetch(`/fragments/${fragment}.html`)
+    fetch(`./fragments/${fragment}.html`)
         .then(data => data.text())
         .then(data => htmlElement.innerHTML = data);
     }
